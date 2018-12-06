a9df1cc4c9b764e2cd8afca116b9ddbd2eca44cc
一匹马赛克 <104416188@qq.com>
2018-12-06 19:51:20 +0800

    new

--- src/components/common/PecooFooter.vue
+++ src/components/common/PecooFooter.vue
@@ -33,7 +33,7 @@ export default {
         },
         {
           title: '奢侈品',
-          link: '/luxury',
+          link: '/luxuryindex',
           default: 'https://static.pecoo.com/images/h5/luxuries.png',
           selected: 'https://static.pecoo.com/images/h5/luxuries_selected.png'
         },
